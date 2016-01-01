c3d9646c4bbb852b472d3558f8b8f6754b82ad22
Sameer <sayyagari@innominds.com>
2016-01-02 00:56:50 +0530

    Adding base files..

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name glanbiotechsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the glanbiotechsApp
+ */
+angular.module('glanbiotechsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
