f3e58ade3b0b892199dca3d0cc29208bd04ffc17
Sameer <sayyagari@innominds.com>
2016-01-03 16:56:51 +0530

    Added index..

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var server = express(); // better instead
 
   //server.use('/media', express.static(__dirname + '/media'));
-  server.use(express.static(__dirname + '/dist'));
+  server.use(express.static(__dirname + '/app'));
 
 
 server.listen(process.env.PORT || 5000);
