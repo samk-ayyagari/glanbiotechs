6fe803940b328ff87637309de9cee8a2a1ae84f7
Sameer <sayyagari@innominds.com>
2016-01-03 14:55:14 +0530

    added prod env in local ...

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic('dist')).listen(8080);
+connect().use(serveStatic('dist')).listen(process.env.PORT || 5000)
