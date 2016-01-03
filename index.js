2d1ad29df9096c6ef9740496b426f0aeac1a682f
Sameer <sayyagari@innominds.com>
2016-01-03 16:11:07 +0530

    Removed configure ..

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 var express = require('express');
+//var server = express.createServer();
+// express.createServer()  is deprecated. 
 var server = express(); // better instead
-server.configure(function(){
-//  server.use('/media', express.static(__dirname + '/media'));
+
+  //server.use('/media', express.static(__dirname + '/media'));
   server.use(express.static(__dirname + '/public'));
-});
+
 
 server.listen(process.env.PORT || 5000);
