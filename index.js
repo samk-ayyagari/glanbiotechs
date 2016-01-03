141f66b698c110421b6a8215885378e510a46dc7
Sameer <sayyagari@innominds.com>
2016-01-03 11:08:06 +0530

    added server

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(5000);
-
-
+connect().use(serveStatic('dist')).listen(8080);
